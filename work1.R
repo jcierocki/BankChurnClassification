52ba08dab61e22fdef37ed54846fe0140499ad7b
Jakub Cierocki <jcierocki@jcierocki-notebook.home>
2020-04-16 11:43:10 +0200

    Added package "scorecard" and sample use of (its) functions calculating IV and optimal binning.

--- work1.R
+++ work1.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(stringr)
 library(ggplot2)
 library(GGally)
+library(scorecard)
 # library(caret)
 # library(ranger)
 
@@ -33,3 +34,10 @@ ggplot(data1, aes(x = Exited, y = Age)) + geom_violin()
 plot_freq(data1, "Geography")
 data1 <- data1 %>% mutate(NotSpain = as.factor(map_chr(Geography, ~ ifelse(.x == "Spain", "No", "Yes"))))
 plot_freq(data1, "NotSpain")
+
+iv(data1, "Exited", "Geography", positive = "No")
+iv(data1, "Exited", "NotSpain", positive = "No")
+
+opt_bin <- woebin(data1, "Exited", "Age", positive = "No")#[[1]]$breaks
+
+data2 <- data1 %>% woebin_ply(opt_bin, to = "bin") %>% mutate(Age_bin = as.factor(Age_bin))
