5e880d2eb4db74eceeb1517974a4f02477db7d78
Jakub Cierocki <jcierocki@jcierocki-notebook.home>
2020-04-17 12:58:15 +0200

    Added functions to merge factor vairables. Tested funModeling package.

--- work1.R
+++ work1.R
@@ -5,6 +5,7 @@ library(stringr)
 library(ggplot2)
 library(GGally)
 library(scorecard)
+library(funModeling)
 # library(caret)
 # library(ranger)
 
@@ -40,4 +41,17 @@ iv(data1, "Exited", "NotSpain", positive = "No")
 
 opt_bin <- woebin(data1, "Exited", "Age", positive = "No")#[[1]]$breaks
 
-data2 <- data1 %>% woebin_ply(opt_bin, to = "bin") %>% mutate(Age_bin = as.factor(Age_bin))
+data2 <- data1 %>% mutate(Age_bin2 = discretize_rgr(Age, Exited, max_n_bins = 6))
+data2 <- data2 %>% woebin_ply(opt_bin, to = "bin") %>% mutate(Age_bin = as.factor(Age_bin))
+
+# opt_bin2 <- discretize_get_bins(data1, 4, input = "Age")
+
+head(data2$Age_bin2)
+plot(data2$Age_bin)
+plot(data2$Age_bin2)
+
+iv(data2, y = "Exited", positive = "No")
+
+#### łączenie zmiennych w celu uzyskania zbliżonej liczby kategorii (nieobciążone oceny ważności parametrów i IV)
+
+# dodałem funkcję w pliku funs.R
---
+++ work2.R
@@ -0,0 +1,4 @@
+##### work file 2 - tidymodels + ranger
+
+library(tidyverse)
+library(stringr)
