eec633adb0ca315149c3894ec4b0382ae94a0838
Jakub Cierocki <jcierocki@jcierocki-notebook.home>
2020-05-05 23:03:56 +0200

    Used package "vip" to mesure variable importance.

--- work2.R
+++ work2.R
@@ -1,31 +1,36 @@
 ##### work file 2 - tidymodels + ranger
-
+  
 library(tidyverse)
 library(stringr)
 library(tidymodels)
 library(ranger)
-
+library(vip)
+  
 source("main.R")
 rm(list = ls()[ls() != "data2"])
 
 dataset_split <- initial_split(data2, prop = 0.75)
 df_train <- dataset_split %>% training()
 df_test <- dataset_split %>% testing()
-
+  
 ranger_model_1 <- rand_forest("classification", 2, 500, 5) %>% 
-  set_engine("ranger", num.threads = 8, replace = F, sample.fraction = 0.8) %>%
+  set_engine("ranger", num.threads = 8, replace = F, sample.fraction = 0.8, importance = "impurity") %>%
+  # set_engine("ranger", num.threads = 8, replace = F, sample.fraction = 0.8, importance = "permutation", local.importance = T) %>%
   # set_engine("ranger", num.threads = 8) %>%
   fit(Exited ~ ., data = df_train)
-
+  
 df_pred <- ranger_model_1 %>% 
   predict(df_test) %>% 
   bind_cols(df_test)
-
+  
 df_pred %>% metrics(Exited, .pred_class)
-
+  
 df_pred_probs <- ranger_model_1 %>% 
   predict(df_test, type = "prob") %>% 
   bind_cols(df_test)
-
+  
 df_pred_probs %>% roc_curve(Exited, .pred_No) %>% autoplot()
+
+vi(ranger_model_1)
+vip(ranger_model_1)
 
