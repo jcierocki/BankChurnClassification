7294c7c5f51dd46a3ed1d184753a7e8baa33b4d0
Jakub Cierocki <jcierocki@jcierocki-notebook.home>
2020-05-10 11:01:03 +0200

    Implemented calculation and export od all, crucial model validation statistics. Actual solution is not fully automated. Exported graphics.

--- validation_rf.R
+++ validation_rf.R
@@ -16,14 +16,34 @@ pred_dfs <- read_rds("data/predictions.RDS")
 source("funs_valid.R")
 
 all_metrics <- get_all_metrics(pred_dfs)
+all_metrics %>%
+  dplyr::select(-2) %>% 
+  kable(format = "html") %>% 
+  save_kable("figures/metrics.png")
 
-pred_dfs[[1]][[1]] %>% exportable_conf_matrix %>% kable(format = "markdown")
+pred_dfs[[1]][[1]] %>% 
+  exportable_conf_matrix %>% 
+  kable(format = "html") %>% 
+  save_kable("figures/conf_matrix1.png")
 
-# df_pred_probs1 %>% roc_curve(Exited, .pred_No) %>% autoplot()
-# df_pred_probs2 %>% roc_curve(Exited, .pred_No) %>% autoplot()
-# 
-# vi(ranger_model_1)
-# vi(ranger_model_2)
-# 
-# vip(ranger_model_1)
-# vip(ranger_model_2)
+pred_dfs[[2]][[1]] %>% 
+  exportable_conf_matrix %>% 
+  kable(format = "html") %>% 
+  save_kable("figures/conf_matrix2.png")
+
+roc_1 <- pred_dfs[[1]][[1]] %>% 
+  roc_curve(Exited, .pred_No) %>% 
+  autoplot()
+
+roc_2 <- pred_dfs[[2]][[1]] %>% 
+  roc_curve(Exited, .pred_No) %>% 
+  autoplot()
+
+ggsave("figures/roc_1.png", roc_1)
+ggsave("figures/roc_2.png", roc_2)
+
+vip_1 <- vip(fitted_models[[1]][[1]])
+vip_2 <- vip(fitted_models[[2]][[1]])
+
+ggsave("figures/vip_1.png", vip_1)
+ggsave("figures/vip_2.png", vip_2)
