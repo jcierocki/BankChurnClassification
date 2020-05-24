272e7c0a9741e96a188c3e30ab9da13f7446b606
Jakub Cierocki <jcierocki@jcierocki-notebook.home>
2020-05-24 19:07:40 +0200

    Exported results for xgboost

--- gbm.R
+++ gbm.R
@@ -5,9 +5,12 @@ library(stringr)
 library(tidymodels)
 library(xgboost)
 library(vip)
+library(knitr)
+library(kableExtra)
 
 rm(list = ls())
 
+source("funs_valid.R")
 # source("dataset_prep.R")
 
 dataset_split <- readRDS("data/split.RDS")
@@ -37,12 +40,29 @@ df_pred <- gbm_model_1 %>%
 
 df_pred %>% metrics(Exited, .pred_class)
 
+############################
+
+df_pred <- gbm_model_1 %>% 
+  predict(df_test, type = "prob") %>% 
+  bind_cols(df_pred)
+
+df_pred %>% 
+  metrics(Exited, .pred_class, .pred_No) %>% 
+  kable("html") %>% 
+  save_kable("figures/conf_matrix_gbm.png")
+
+############################
+
 df_pred_probs <- gbm_model_1 %>% 
   predict(df_test, type = "prob") %>% 
   bind_cols(df_test)
 
-df_pred_probs %>% roc_auc(Exited, .pred_No)
+# df_pred_probs %>% roc_auc(Exited, .pred_No)
 df_pred_probs %>% roc_curve(Exited, .pred_No) %>% autoplot()
+ggsave("figures/roc_gbm.png")
 
 vi(gbm_model_1)
 vip(gbm_model_1)
+# vip(gbm_model_1, method = "permute", train = df_train, target = "Exited", metric = "accuracy", pred_wrapper = predict)
+
+ggsave("figures/vip_gbm.png")
